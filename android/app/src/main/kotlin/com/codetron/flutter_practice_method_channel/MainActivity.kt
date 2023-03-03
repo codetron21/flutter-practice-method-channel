38223f9b76fc979cc5bbbb25d9b6c11c0ebebcae
codetron <codetronclan@gmail.com>
2023-03-03 21:20:21 +0700

    update dev serial

---
+++ android/app/src/main/kotlin/com/codetron/flutter_practice_method_channel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codetron.flutter_practice_method_channel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,23 +8,23 @@ import Flutter
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
         let serialChannel = FlutterMethodChannel(name: "com.codetron/serial",
                                                  binaryMessenger: controller.binaryMessenger)
         
         serialChannel.setMethodCallHandler({
             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-           
+            
             guard call.method == "getDevSerial" else {
                 result(FlutterMethodNotImplemented)
                 return
-              }
+            }
             
             result(UIDevice.current.identifierForVendor?.uuidString)
-
         })
         
-        GeneratedPluginRegistrant.register(with: self)
         
+        GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
